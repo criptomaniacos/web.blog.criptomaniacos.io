a1177d15dd04eddc67e58074367f28cca24a5c3a
Pedro Moreno <nennogabriel@gmail.com>
2023-12-14 18:03:18 -0300

    basic

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,48 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
+
+const colors = (() => {
+  const colors = {
+    brand: {
+      black: "#181818",
+      green: {
+        500: "#05B97E",
+        DEFAULT: "#05B97E",
+      },
+      purple: {
+        500: "#7F36A7",
+        DEFAULT: "#7F36A7",
+      },
+      pink: {
+        500: "#BC017E",
+        DEFAULT: "#BC017E",
+      },
+      orange: {
+        500: "#F5803E",
+        DEFAULT: "#F5803E",
+      },
+    },
+  };
+
+  return {
+    ...colors,
+  };
+})();
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        ...colors,
       },
     },
   },
-  plugins: [],
-}
-export default config
+  plugins: [require("@tailwindcss/typography")],
+};
+export default config;
+
+//https://s.cmania.co/guru/95c3cfef-3fdc-477d-b8cc-7bf0b2d659d4?name=[field id="fname"]&amp;email=[field id="femail"]&amp;nome=[field id="fname"]
